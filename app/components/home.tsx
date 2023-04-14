a6a6d7fc649f24a572d2844f8f0046d6584e7260
yudsf <130756205+yudsf@users.noreply.github.com>
2023-04-14 19:07:55 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -167,9 +167,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>Terry's chatGPT</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            My own AI assistant.
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
@@ -205,7 +205,7 @@ function _Home() {
               />
             </div>
             <div className={styles["sidebar-action"]}>
-              <a href={REPO_URL} target="_blank">
+              <a href="https://www.gptplus.wiki" target="_blank">
                 <IconButton icon={<GithubIcon />} shadow />
               </a>
             </div>
